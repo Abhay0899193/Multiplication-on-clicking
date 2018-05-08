66ab81b7fbbabc47d291e45bce30d18c8e0f6540
Abhay <abhayooosing@gmail.com>
2018-05-08 15:39:48 +0530

    new

--- build.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
- emcc /exam/multiply.cpp --js-library /exam/multiply_in.js -o /exam/index.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]'
